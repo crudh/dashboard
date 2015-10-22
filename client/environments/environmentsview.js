a014a9a432980f1235ad915edfa8d6e777a732c6
Christian Rudh <christian@rudh.se>
2015-10-22 06:30:25 +0200

    Cleanups.

--- client/environments/environmentsview.js
+++ client/environments/environmentsview.js
@@ -1,27 +1,34 @@
 import React, {Component, PropTypes} from "react";
 
-export default class EnvironmentsView extends Component {
-  renderTitle(item) {
-    const {setEnvironment, active} = this.props;
-    if (item.id === active.id) return item.name;
+class TitleView extends Component {
+  render() {
+    const {setEnvironment, env, activeId} = this.props;
+    if (env.id === activeId) {
+      return <span>{env.name}</span>;
+    }
 
     return (
-      <a href="#" onClick={() => setEnvironment(item.id)}>
-        {item.name}
+      <a href="#" onClick={() => setEnvironment(env.id)}>
+        {env.name}
       </a>
     );
   }
+}
 
+export default class EnvironmentsView extends Component {
   render() {
-    const {list} = this.props;
+    const {setEnvironment, list, active} = this.props;
     if (!list) return (
       <span>No environments found</span>
     );
 
-    const environmentList = list.map(item => {
+    const environmentList = list.map(env => {
       return (
-        <li key={item.id}>
-          {this.renderTitle(item)}
+        <li key={env.id}>
+          <TitleView
+            env={env}
+            activeId={active.id}
+            setEnvironment={setEnvironment}/>
         </li>
       );
     });
