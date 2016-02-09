4d0143ecb9f43660e0ede98ee780917563c04c7b
Christian Rudh <christian@rudh.se>
2016-02-09 22:20:31 +0100

    Removed duplicate code that belongs in checksactions.

--- client/actions/environmentsactions.js
+++ client/actions/environmentsactions.js
@@ -1,9 +1,4 @@
-import { checkStatus } from "../core/statuses";
-
 export const SET_ENVIRONMENT = "SET_ENVIRONMENT";
-export const FETCH_CHECKS = "FETCH_CHECKS";
-export const FETCH_CHECKS_COMPLETED = "FETCH_CHECKS_COMPLETED";
-export const FETCH_CHECKS_FAILED = "FETCH_CHECKS_FAILED";
 
 export function setEnvironment(id) {
   return {
@@ -11,39 +6,5 @@ export function setEnvironment(id) {
     payload: {
       id
     }
-  };
-}
-
-function fetchChecksFromServer() {
-  return fetch("http://localhost:3000/services/checks");
-}
-
-function fetchChecksCompleted(checks) {
-  return {
-    type: FETCH_CHECKS_COMPLETED,
-    checks
-  };
-}
-
-function fetchChecksFailed(error) {
-  return {
-    type: FETCH_CHECKS_FAILED,
-    error
-  };
-}
-
-export function fetchChecks() {
-  return dispatch => {
-    dispatch({
-      type: FETCH_CHECKS
-    });
-
-    return fetchChecksFromServer()
-      .then(checkStatus)
-      .then(response => response.json())
-      .then(json => dispatch(fetchChecksCompleted(json)))
-      .catch(error => {
-        dispatch(fetchChecksFailed(error.message));
-      });
   };
 }
